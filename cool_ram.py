6b4136ef1d14ffca690b4ea032a748a394a33fe1
paulmeyer <pmeyer@meyerzone.net>
2018-10-22 13:40:58 -0500

    added more options: -nwr/-nwt (independent stabilization control at ram and new filament), delete temp change during cooled (with override), default now does m104 before TC, m109 after if stabilization desired

--- cool_ram.py
+++ cool_ram.py
@@ -8,18 +8,28 @@ import os        # os routines for reading/writing files
 parser = argparse.ArgumentParser(description='Simple script to hack the gcode from Prusa Slic3r v1.41.0 to drop temp during ram')
 
 # add the arguments
-parser.add_argument("-t", "--ram_temp",help="Ram temperature",type=int, default=180)
-parser.add_argument("-nw", "--no_wait",    help="don't wait for stable temp",action="store_true")
-parser.add_argument("-i", "--input",   help="input file", required=True)
-parser.add_argument("-o", "--output",  help="output file", default="none")
+parser.add_argument("-t",    "--ram_temp",              help="Ram temperature",type=int, default=180)
+parser.add_argument("-nwr",  "--no_wait_ram",           help="don't wait for stable temp when starting the ram sequence",action="store_true")
+parser.add_argument("-nwt",  "--no_wait_tc",            help="don't wait for stable temp before printing with the new filament (after the tool change)",action="store_true")
+parser.add_argument("-fs",   "--full_stabilization_tc", help="wait for full stable temperature before initiating the tool change",action="store_true")
+parser.add_argument("--temp_change_override",           help="Retain the Slic3r inserted temperature after ramming and before tool change (default false) ",action="store_true")
+parser.add_argument("-i",    "--input",                 help="input file", required=True)
+parser.add_argument("-o",    "--output",                help="output file", default="none")
 
 # pull in the command line and parse all the arguments into the 'args' variable
 args = parser.parse_args()
 
 
-# turn the parsed arguments into clearly local variables
-no_wait   = args.no_wait
-ram_temp  = args.ram_temp
+# turn the parsed arguments into clearly labelled local variables
+ram_temp               = args.ram_temp
+full_stabilization_tc  = args.full_stabilization_tc
+no_wait_ram            = args.no_wait_ram
+temp_change_override   = args.temp_change_override
+
+if(full_stabilization_tc):
+    no_wait_tc = False
+else:
+    no_wait_tc = args.no_wait_tc
 
 # get the input file specified, and turn it into a path variable for the current OS
 inpath    = os.path.normpath(args.input)
@@ -46,7 +56,7 @@ start          = r"^; CP TOOLCHANGE START"
 
 # if we are waiting, us M109.  Otherwise, M104.  This will be added
 #  to the file just after the CP TOOLCHANGE START line
-if(no_wait):
+if(no_wait_ram):
     start_addition = "M104 S%d ; set temp for Ram cooling, no wait\n"
 else:    
     start_addition = "M109 R%d ; set temp for Ram cooling\n"
@@ -55,12 +65,28 @@ else:
 # the string that indicates the actual tool change (unload/load, after the ram)
 end            = r"^T[0-9]"
 
-# if we are waiting, us M109.  Otherwise, M104.  This will be added
-#  to the file just before the "T" toolchange line
-if(no_wait):
-    end_addition   = "M104 S%s  ; restore temperature\n"
-else:    
-    end_addition   = "M109 R%s  ; restore temperature\n"
+
+# Three options for the end:
+
+# -full_stabilization_tc specified
+#    m109 before tool change
+#
+# default (no options)
+#    m104 before tool change
+#    m109 after  tool change
+
+# -no_wait_tc specified
+#    m104 before tool change
+
+if(full_stabilization_tc):
+    pre_tc_addition  = "M109 R%s  ; restore temperature, stabilize before TC\n"
+    post_tc_addition = ""
+elif(no_wait_tc):
+    pre_tc_addition  = "M104 S%s  ; restore temperature\n"
+    post_tc_addition = ""
+else:    # default
+    pre_tc_addition  = "M104 S%s  ; restore temperature before TC\n"
+    post_tc_addition = "M109 R%s  ; restore temperature stabilize after TC\n"
 
 # string to check for any temperature changes.  Finds either m104 or m109, allows grabbing the temp
 temperature_set = r"^M10[49] S([0-9]*)"
@@ -93,12 +119,20 @@ for line in infile:
         # "group(1)" of the temp_set_match will contain the
         #    temperature being set as a string variable ("200" or whatever)
         current_temp = temp_set_match.group(1)
-
-        # output the temperature set line to the output file
-        outfile.write(line)
+        
         # output a comment line indicating we grabbed this temperature (debugging only)
         outfile.write(";matched temp! :" +  current_temp + "\n")
 
+        # if this temperature change is happening in the 'cooled' state,
+        #  it is adjusting the temperature for the next filament, and
+        #  likely doing it too soon.  We'll delete that and depend on the
+        #  temperature change near the actual tool change
+
+        # if we aren't in cooled state, print the temperature change
+        #   if we are in cooled state, but the temp_change_override option is set, print the temperature change
+        if(  (state != "cooled") or (temp_change_override)):
+            # output the temperature set line to the output file
+            outfile.write(line)
 
     # if we are in idle state and match the 'start' pattern        
     elif (state == "idle") and (start_match is not None):
@@ -117,10 +151,14 @@ for line in infile:
 
         # output the "restore temperature" string, with the most recently
         #    detected temperature in the original gcode
-        outfile.write(end_addition % current_temp)
+        outfile.write(pre_tc_addition % current_temp)
 
         # don't forget to output the tool change line
         outfile.write(line)
+
+        # output the "stabilization" temp if needed
+        if(post_tc_addition != ""):
+            outfile.write(post_tc_addition % current_temp)
 
         # go back to printing state (normal temperature)
         state = "idle"
